b0f110b55d348a15b915aaa838948a8b22c6186f
Atif Murtaza Mahmud <atifmahmud95@gmail.com>
2024-11-06 21:48:42 -0800

    basic player state

---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState
+{
+    public GameManager.GridSquare position;
+    public bool isPossessing;
+    PlayerAttribute playerAttribute;
+}
