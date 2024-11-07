c0f7dce2ebd0e1184c8a67c023f4fae0cada1966
Atif Murtaza Mahmud <atifmahmud95@gmail.com>
2024-11-07 14:25:09 -0800

    Ball func to know position

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    private float kickForce = 60f;
     public float deceleration = 0.9f;
     public GameManager.GridSquare position;    
+    
     private Vector2 velocity;
+    private float kickForce = 60f;
+    private int ownerInstanceId;
 
     public void Kick(Vector2 direction)
     {
+        Debug.Log("Starting from: " + GetCurrentSquare());
         StopAllCoroutines();
         StartCoroutine(Move(direction));
     }
@@ -20,12 +23,26 @@ public class Ball : MonoBehaviour
         velocity = direction * kickForce;
         Debug.Log("Velocity is: " + velocity);
         Debug.Log("Direction is: " + direction);
-        while (velocity.magnitude > 0.00001f)
+        while (velocity.magnitude > 0.2f)
         {
             transform.position += new Vector3(velocity.x, 0, velocity.y) * Time.deltaTime;
             velocity *= deceleration;
             yield return new WaitForSeconds(0.02f);
         }
         velocity = Vector2.zero;
+        Debug.Log("Ended up in: " + GetCurrentSquare());
+    }
+
+    private string GetCurrentSquare()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -transform.up, out hit))
+        {
+            if (hit.transform.CompareTag("Square"))
+            {
+                return hit.transform.gameObject.name;
+            }
+        }
+        return "none";
     }
 }
